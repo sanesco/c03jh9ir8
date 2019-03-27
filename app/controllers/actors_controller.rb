1010890bda31bfc27c325a1659f6f8d2039568e0
sanesco <santiagoescobar11z@gmail.com>
2019-03-27 12:02:50 -0500

    Creando actores

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,19 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
+  def new
+    @actors = Actor.new
+  end
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to actors_path
+    else
+      render :new
+    end
+  end
+  private
+  def actor_params
+    params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :alive, :death_date, :death_place)
+  end
 end
